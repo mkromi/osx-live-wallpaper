008a900dd4ac59b628a4af70ca1c0f31b1dad0a8
Mohiuddin Khan <mohiuddinkhanromi@gmail.com>
2022-10-31 02:40:56 +0600

    bug fixed to multiple monitors

--- app.sh
+++ app.sh
@@ -3,6 +3,9 @@
 Xaxis=$(/usr/sbin/system_profiler SPDisplaysDataType | awk '/Resolution/{print $2}')
 Yaxis=$(/usr/sbin/system_profiler SPDisplaysDataType | awk '/Resolution/{print $4}')
 
+splitXaxis=(${Xaxis// / })
+splitYaxis=(${Yaxis// / })
+
 tags=$(/opt/homebrew/bin/jq -r '.tags' $PWD/osx-live-wallpaper/data.json)
 
 IFS=', ' read -r -a array <<< "$tags"
@@ -17,6 +20,6 @@ fi
 echo -e "GET https://source.unsplash.com HTTP/1.0\n\n" | nc source.unsplash.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-    curl -L https://source.unsplash.com/${Xaxis}x${Yaxis}?${array[$index]} --output $PWD/osx-live-wallpaper/$file
+    curl -L https://source.unsplash.com/${splitXaxis}x${splitYaxis}?${array[$index]} --output $PWD/osx-live-wallpaper/$file
     osascript -e 'tell application "System Events" to tell every desktop to set picture to "'$PWD/osx-live-wallpaper/$file'"'
 fi
